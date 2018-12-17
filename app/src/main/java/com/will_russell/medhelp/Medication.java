403af3d423c0c86f9122344e42466e83d1e95ba6
wrussell1999 <jetpowerss@gmail.com>
2018-12-17 00:48:14 +0000

    Design updated

---
+++ app/src/main/java/com/will_russell/medhelp/Medication.java
@@ -0,0 +1,75 @@
+package com.will_russell.medhelp;
+
+import java.util.ArrayList;
+
+public class Medication {
+    private String name;
+    private int timesTaken;
+    private Double doseSize;
+    private int length;
+    private Integer quantity;
+
+    public static ArrayList<Medication> medicationList = new ArrayList<Medication>();
+
+    public Medication(String name, int timesTaken, Double doseSize, int length) {
+        this.name = name;
+        this.timesTaken = timesTaken;
+        this.doseSize = doseSize;
+        this.length = length;
+    }
+
+    // Constructor for when medication is until otherwise stated
+    public Medication(String name, int timesTaken, Double doseSize) {
+        this.name = name;
+        this.timesTaken = timesTaken;
+        this.doseSize = doseSize;
+    }
+
+    public Medication(String name, int timesTaken, Integer quantity) {
+        this.name = name;
+        this.timesTaken = timesTaken;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getTimesTaken() {
+        return this.timesTaken;
+    }
+    public Double getDoseSize() {
+        return this.doseSize;
+    }
+    public int getLength() {
+        return this.length;
+    }
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+
+    public void setName(String name) {
+        this.name= name;
+    }
+    public void setTimesTaken(int timesTaken) {
+        this.timesTaken = timesTaken;
+    }
+    public void setDoseSize(Double dose) {
+        this.doseSize = dose;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean validQuantity() {
+        if (this.quantity != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
